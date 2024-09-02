a1d8bf85b3bc8b0a3841d237c4ed15da6c77c167
Meet Kalathiya <meetkalathiya012@gmail.com>
2024-09-02 15:13:40 +0530

    header btn UI changed

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
               {userData?.role === "admin" && token && (
                 <button
                   onClick={() => navigate("/permission")}
-                  className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
+                  className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white"
                 >
                   <span className="relative px-5 text-gray-900 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-white-900 rounded-md group-hover:bg-opacity-0 group-hover:text-white">
                     Dashboard
@@ -39,7 +39,6 @@ function App() {
               )}
               {token && (
                 <button
-                  className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
                   onClick={() => {
                     localStorage.removeItem("token");
                     dispatch(setToken(null));
@@ -47,6 +46,7 @@ function App() {
                     navigate("/login");
                     toast.success("/Logout successfully!!!");
                   }}
+                  className="text-white bg-[#2557D6] hover:bg-[#2557D6]/90 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#2557D6]/50 mb-2"
                 >
                   Logout
                 </button>
